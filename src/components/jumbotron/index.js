e335bd4be96364ad523a95d67a22f49013f34d11
imberlyarboe <kimberjarboe37@gmail.com>
2020-12-09 14:58:27 -0600

    Create repo of existing project - Netflix Clone with React, Set up Jumbotron

---
+++ src/components/jumbotron/index.js
@@ -0,0 +1,47 @@
+/* eslint-disable react/jsx-no-undef */
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/jsx-props-no-spreading */
+/* eslint-disable linebreak-style */
+import React from 'react';
+import {
+  Container,
+  Inner,
+  Item,
+  Pane,
+  Title,
+  SubTitle,
+  Image,
+} from './styles/jumbotron';
+
+export default function Jumbotron({
+  children,
+  direction = 'row',
+  ...restProps
+}) {
+  return (
+    <Item>
+      <Inner direction={direction}>{children}</Inner>
+    </Item>
+  );
+}
+
+Jumbotron.Container = function JumbotronContainer({ children, ...restProps }) {
+  return <Container {...restProps}>{children}</Container>;
+};
+
+Jumbotron.Pane = function JumbotronPane({ children, ...restProps }) {
+  return <Pane {...restProps}>{children}</Pane>;
+};
+
+Jumbotron.Title = function JumbotronTitle({ children, ...restProps }) {
+  return <Title {...restProps}>{children}</Title>;
+};
+
+Jumbotron.SubTitle = function JumbotronSubTitle({ children, ...restProps }) {
+  return <SubTitle {...restProps}>{children}</SubTitle>;
+};
+
+Jumbotron.Image = function JumbotronImage({ ...restProps }) {
+  return <Image {...restProps} />;
+};
